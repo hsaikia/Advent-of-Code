d715f32a0b63266a1f94509b1e684342ba72bfaf
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-07 08:12:00 +0100

    minor

--- src/bin/2023_07/main.rs
+++ src/bin/2023_07/main.rs
@@ -79,9 +79,9 @@ fn get_hand_type(hand: &str, joker: bool) -> HandType {
     hand_type_from_sizes(num_unique_cards, largest_set_size)
 }
 
-fn compare_hands(hand1: &str, hand2: &str, part1: bool) -> Ordering {
-    let t1 = get_hand_type(hand1, !part1) as usize;
-    let t2 = get_hand_type(hand2, !part1) as usize;
+fn compare_hands(hand1: &str, hand2: &str, joker: bool) -> Ordering {
+    let t1 = get_hand_type(hand1, joker) as usize;
+    let t2 = get_hand_type(hand2, joker) as usize;
 
     let order: Ordering = t1.cmp(&t2);
 
@@ -93,7 +93,7 @@ fn compare_hands(hand1: &str, hand2: &str, part1: bool) -> Ordering {
             if c1 == c2 {
                 continue;
             }
-            let card_order = if part1 { ORDER_1 } else { ORDER_2 };
+            let card_order = if joker { ORDER_2 } else { ORDER_1 };
             let idx1 = card_order.iter().position(|c| c == c1).unwrap();
             let idx2 = card_order.iter().position(|c| c == c2).unwrap();
 
@@ -107,7 +107,7 @@ fn compare_hands(hand1: &str, hand2: &str, part1: bool) -> Ordering {
     order
 }
 
-fn solve(input: &str, part1: bool) {
+fn solve(input: &str, joker: bool) {
     let mut cards: Vec<(&str, usize)> = Vec::new();
 
     for line in input.split('\n') {
@@ -115,7 +115,7 @@ fn solve(input: &str, part1: bool) {
         cards.push((hand_bid[0], io::parse_num::<usize>(hand_bid[1]).unwrap()));
     }
 
-    cards.sort_by(|c1, c2| compare_hands(c2.0, c1.0, part1));
+    cards.sort_by(|c1, c2| compare_hands(c2.0, c1.0, joker));
 
     let ans = cards
         .iter()
@@ -123,7 +123,7 @@ fn solve(input: &str, part1: bool) {
         .map(|(i, e)| (i + 1) * e.1)
         .sum::<usize>();
 
-    if part1 {
+    if !joker {
         println!("Answer Part 1 {}", ans);
     } else {
         println!("Answer Part 2 {}", ans);
@@ -133,7 +133,7 @@ fn solve(input: &str, part1: bool) {
 fn main() {
     for input in INPUT {
         println!("{}", input.0);
-        solve(input.1, true);
         solve(input.1, false);
+        solve(input.1, true);
     }
 }
