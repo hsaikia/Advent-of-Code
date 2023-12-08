335aaeb82ca9e1baa43251d08505a4a7607f9873
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-08 19:54:33 +0100

    2022.13

--- src/bin/2022_13/main.rs
+++ src/bin/2022_13/main.rs
@@ -1,77 +1,134 @@
-const INPUT: [(&str, &str); 1] = [
+use std::cmp::Ordering;
+
+use aoc::io;
+
+const INPUT: [(&str, &str); 2] = [
     ("Sample Input", include_str!("sample_input.txt")),
-    //("Input", include_str!("input.txt")),
+    ("Input", include_str!("input.txt")),
 ];
 
-fn check(str1: &str, str2: &str) -> bool {
-    println!("Checking {} and {}", str1, str2);
-
-    if str1 == str2 {
-        return true;
-    }
-
-    if str1.find('[').is_none()
-        && str1.find(']').is_none()
-        && str1.find(',').is_none()
-        && str2.find('[').is_none()
-        && str2.find(']').is_none()
-        && str2.find(',').is_none()
-    {
-        let n1 = str1.parse::<usize>().unwrap();
-        let n2 = str2.parse::<usize>().unwrap();
-
-        return n1 <= n2;
-    }
-
-    let l1 = str1.len();
-    let l2 = str2.len();
-
-    if l1 == 0 && l2 > 0 {
-        return true;
-    }
-
-    let mut str1 = str1;
-    let mut str2 = str2;
-    if str1.starts_with('[') && str1.chars().nth(l1 - 1).unwrap() == ']' {
-        str1 = &str1[1..l1 - 1];
-    }
-    if str2.starts_with('[') && str2.chars().nth(l2 - 1).unwrap() == ']' {
-        str2 = &str2[1..l2 - 1];
-    }
-    let elems1 = str1.split(',').collect::<Vec<&str>>();
-    let elems2 = str2.split(',').collect::<Vec<&str>>();
-
-    let l_min = elems1.len().min(elems2.len());
-
-    for i in 0..l_min {
-        let res = check(elems1[i], elems2[i]);
-        if !res {
-            return false;
-        }
-    }
-
-    true
+#[derive(Debug, Clone)]
+enum Packet {
+    List(Vec<Packet>),
+    Num(u32),
 }
 
-fn part1(input: &str) {
-    let mut idx = 0;
-    let mut ans = 0;
-    let input_lines = input.split('\n').collect::<Vec<_>>();
-    while idx < input_lines.len() {
-        println!("Checking Pair {}", idx / 2 + 1);
-        if check(input_lines[idx], input_lines[idx + 1]) {
-            println!("Correct Order");
-            ans += 1;
-        }
-        idx += 3;
+fn compare(p1: &Packet, p2: &Packet) -> Ordering {
+    match p1 {
+        Packet::List(v1) => match p2 {
+            Packet::List(v2) => {
+                for (x, y) in v1.iter().zip(v2.iter()) {
+                    if compare(x, y) == Ordering::Equal {
+                        continue;
+                    }
+                    return compare(x, y);
+                }
+                v1.len().cmp(&v2.len())
+            }
+            Packet::Num(n2) => compare(p1, &Packet::List(vec![Packet::Num(*n2)])),
+        },
+        Packet::Num(n1) => match p2 {
+            Packet::List(_) => compare(&Packet::List(vec![Packet::Num(*n1)]), p2),
+            Packet::Num(n2) => n1.cmp(n2),
+        },
     }
+}
+
+fn parse_packet(packet_str: &str) -> Packet {
+    let mut open = Vec::new();
+    let mut packets = Vec::new();
+
+    if packet_str.is_empty() {
+        return Packet::List(Vec::new());
+    }
+
+    let chs = packet_str.chars().collect::<Vec<_>>();
+
+    let mut num: Option<u32> = None;
+    for (i, ch) in chs.iter().enumerate() {
+        if *ch == '[' {
+            open.push(i);
+        } else if *ch == ']' {
+            let x = open.pop().unwrap();
+            if open.is_empty() {
+                packets.push(parse_packet(&packet_str[x + 1..i]));
+            }
+        } else if ch.is_ascii_digit() {
+            let d = ch.to_digit(10).unwrap();
+            if open.is_empty() {
+                if let Some(n) = &mut num {
+                    *n = 10 * *n + d;
+                } else {
+                    num = Some(d);
+                }
+            }
+        } else if *ch == ',' {
+            if let Some(n) = &mut num {
+                if open.is_empty() {
+                    packets.push(Packet::Num(*n));
+                }
+                num = None;
+            }
+        }
+    }
+
+    if let Some(n) = num {
+        packets.push(Packet::Num(n));
+    }
+
+    Packet::List(packets)
+}
+
+fn part1(packet_pairs: &[(Packet, Packet)]) {
+    let ans = packet_pairs
+        .iter()
+        .enumerate()
+        .filter_map(|(i, (p1, p2))| {
+            if compare(p1, p2) == Ordering::Less {
+                Some(i + 1)
+            } else {
+                None
+            }
+        })
+        .sum::<usize>();
+
     println!("Part 1 Answer is {}", ans);
 }
 
+fn part2(packet_pairs: &[(Packet, Packet)]) {
+    let mut packets = Vec::new();
+    for (a, b) in packet_pairs {
+        packets.push((a.clone(), 0));
+        packets.push((b.clone(), 0));
+    }
+
+    packets.push((parse_packet("[[2]]"), 1));
+    packets.push((parse_packet("[[6]]"), 1));
+
+    packets.sort_by(|a, b| compare(&a.0, &b.0));
+
+    let ans = packets
+        .iter()
+        .enumerate()
+        .map(|(i, p)| (i + 1) * p.1)
+        .filter(|x| *x > 0)
+        .collect::<Vec<_>>();
+    println!("Part 2 Answer is {}", ans[0] * ans[1]);
+}
+
 fn main() {
-    for input in INPUT {
-        println!("{}", input.0);
-        part1(input.1);
-        //part2(input.1);
+    for (file, input) in INPUT {
+        println!("{}", file);
+
+        let packet_str_pairs = io::line_batches(input);
+        let mut packet_pairs: Vec<(Packet, Packet)> = Vec::new();
+        for packet_str_pair in packet_str_pairs {
+            let p1 = parse_packet(packet_str_pair[0]);
+            let p2 = parse_packet(packet_str_pair[1]);
+            packet_pairs.push((p1, p2));
+        }
+
+        part1(&packet_pairs);
+        part2(&packet_pairs);
     }
 }
