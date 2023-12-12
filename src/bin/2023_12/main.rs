327838ef4c742fdba2de33b29e1b343c0aca25a5
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-12 18:40:14 +0100

    minor refactor

--- src/bin/2023_12/main.rs
+++ src/bin/2023_12/main.rs
@@ -8,23 +8,14 @@ const INPUT: [(&str, &str); 2] = [
 ];
 
 fn expand(arr: &[char], nums: &[usize]) -> (Vec<char>, Vec<usize>) {
-    let mut arr1 = arr.to_owned();
-    arr1.push('?');
-    arr1.extend(arr.to_owned());
-    arr1.push('?');
-    arr1.extend(arr.to_owned());
-    arr1.push('?');
-    arr1.extend(arr.to_owned());
-    arr1.push('?');
-    arr1.extend(arr.to_owned());
-
-    let mut nums1 = nums.to_owned();
-    nums1.extend(nums.to_owned());
-    nums1.extend(nums.to_owned());
-    nums1.extend(nums.to_owned());
-    nums1.extend(nums.to_owned());
-
-    (arr1, nums1)
+    let mut arr5 = arr.to_owned();
+    let mut nums5 = nums.to_owned();
+    for _ in 0..4 {
+        arr5.push('?');
+        arr5.extend(arr.to_owned());
+        nums5.extend(nums.to_owned());
+    }
+    (arr5, nums5)
 }
 
 fn cnt(s: &[char], ch: char) -> usize {
@@ -33,16 +24,21 @@ fn cnt(s: &[char], ch: char) -> usize {
 
 #[derive(Hash, PartialEq, Eq)]
 struct Id {
-    sl: usize,
-    nl: usize,
-    hs: usize,
+    arr_len: usize,
+    num_len: usize,
+    hashes_seen_so_far: usize,
+}
+
+fn insert_in_cache_and_return(map: &mut HashMap<Id, usize>, id: Id, ret: usize) -> usize {
+    map.insert(id, ret);
+    ret
 }
 
 fn solve(s: Vec<char>, mut n: Vec<usize>, hashes: usize, mp: &mut HashMap<Id, usize>) -> usize {
     let id = Id {
-        sl: s.len(),
-        nl: n.len(),
-        hs: hashes,
+        arr_len: s.len(),
+        num_len: n.len(),
+        hashes_seen_so_far: hashes,
     };
 
     if let Some(x) = mp.get(&id) {
@@ -51,62 +47,49 @@ fn solve(s: Vec<char>, mut n: Vec<usize>, hashes: usize, mp: &mut HashMap<Id, us
 
     if n.is_empty() {
         if cnt(&s, '#') == 0 {
-            mp.insert(id, 1);
-            return 1;
+            return insert_in_cache_and_return(mp, id, 1);
         } else {
-            mp.insert(id, 0);
-            return 0;
+            return insert_in_cache_and_return(mp, id, 0);
         }
     }
 
     if s.is_empty() && n.len() == 1 && n[0] == hashes {
-        mp.insert(id, 1);
-        return 1;
+        return insert_in_cache_and_return(mp, id, 1);
     }
 
     if s.is_empty() {
-        mp.insert(id, 0);
-        return 0;
+        return insert_in_cache_and_return(mp, id, 0);
     }
 
     if s[0] == '#' {
         if n[0] == hashes {
-            mp.insert(id, 0);
-            return 0;
+            return insert_in_cache_and_return(mp, id, 0);
         }
-
         let ret = solve(s[1..].to_vec(), n.clone(), hashes + 1, mp);
-        mp.insert(id, ret);
-        return ret;
+        return insert_in_cache_and_return(mp, id, ret);
     } else if s[0] == '.' {
         if n[0] == hashes {
             n.remove(0);
             let ret = solve(s[1..].to_vec(), n.clone(), 0, mp);
-            mp.insert(id, ret);
-            return ret;
+            return insert_in_cache_and_return(mp, id, ret);
         } else if hashes > 0 && hashes < n[0] {
-            mp.insert(id, 0);
-            return 0;
+            return insert_in_cache_and_return(mp, id, 0);
         } else {
             let ret = solve(s[1..].to_vec(), n.clone(), 0, mp);
-            mp.insert(id, ret);
-            return ret;
+            return insert_in_cache_and_return(mp, id, ret);
         }
     } else if s[0] == '?' {
         if n[0] == hashes {
             n.remove(0);
             let ret = solve(s[1..].to_vec(), n.clone(), 0, mp);
-            mp.insert(id, ret);
-            return ret;
+            return insert_in_cache_and_return(mp, id, ret);
         } else if hashes == 0 {
             let dph = solve(s[1..].to_vec(), n.clone(), 1, mp);
             let dpd = solve(s[1..].to_vec(), n.clone(), 0, mp);
-            mp.insert(id, dpd + dph);
-            return dpd + dph;
+            return insert_in_cache_and_return(mp, id, dpd + dph);
         } else {
             let ret = solve(s[1..].to_vec(), n.clone(), hashes + 1, mp);
-            mp.insert(id, ret);
-            return ret;
+            return insert_in_cache_and_return(mp, id, ret);
         }
     }
 
@@ -117,7 +100,6 @@ fn process(input: &str, part1: bool) {
     let mut ans: usize = 0;
 
     for line in input.split('\n') {
-        //println!("{}", line);
         let (arr, nums) = line.split_once(' ').unwrap();
         let arr = arr.chars().collect::<Vec<_>>();
         let nums = io::tokenize(nums, ",")
@@ -126,19 +108,17 @@ fn process(input: &str, part1: bool) {
             .collect::<Vec<_>>();
 
         // Expand
-        if !part1 {
-            let (arr, nums) = expand(&arr, &nums);
-            let mut mp: HashMap<Id, usize> = HashMap::new();
-            let dp_sol = solve(arr.clone(), nums.clone(), 0, &mut mp);
-            ans += dp_sol;
+        let (arr, nums) = if !part1 {
+            expand(&arr, &nums)
         } else {
-            let mut mp: HashMap<Id, usize> = HashMap::new();
-            let dp_sol = solve(arr.clone(), nums.clone(), 0, &mut mp);
-            ans += dp_sol;
-        }
+            (arr, nums)
+        };
+        let mut mp: HashMap<Id, usize> = HashMap::new();
+        let dp_sol = solve(arr.clone(), nums.clone(), 0, &mut mp);
+        ans += dp_sol;
     }
 
-    println!("Answer : {}", ans);
+    println!("Answer Part {} : {}", if part1 { 1 } else { 2 }, ans);
 }
 
 fn main() {
