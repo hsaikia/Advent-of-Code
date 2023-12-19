94ab6a2334521f375a21fa79e78db61c6406379c
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-19 20:41:18 +0100

    2023.19 refactor to use range instead of range union

--- src/bin/2023_19/main.rs
+++ src/bin/2023_19/main.rs
@@ -2,8 +2,11 @@ use std::collections::HashMap;
 
 use aoc::{common, io, range::Range};
 
-fn cat_to_idx(cat: &str) -> usize {
-    match cat {
+const MIN_INC_RANGE: i64 = 1;
+const MAX_EXC_RANGE: i64 = 4001;
+
+fn category_to_idx(category: &str) -> usize {
+    match category {
         "x" => 0,
         "m" => 1,
         "a" => 2,
@@ -19,20 +22,25 @@ enum Action<'a> {
     SendTo(&'a str),
 }
 
+#[derive(Clone, Debug, PartialEq)]
+enum Cmp {
+    Less,
+    More,
+}
+
 #[derive(Clone, Debug)]
 enum Rule<'a> {
-    IfLess([i64; 4], Action<'a>),
-    IfMore([i64; 4], Action<'a>),
+    Conditional([i64; 4], Action<'a>, Cmp),
     Process(Action<'a>),
 }
 
-fn get_action(rule: &str) -> Action<'_> {
-    if rule == "A" {
+fn get_action(res: &str) -> Action<'_> {
+    if res == "A" {
         Action::Accepted
-    } else if rule == "R" {
+    } else if res == "R" {
         Action::Rejected
     } else {
-        Action::SendTo(rule)
+        Action::SendTo(res)
     }
 }
 
@@ -41,14 +49,14 @@ fn parse_rule<'a>(line: &'a str, map: &mut HashMap<&'a str, Vec<Rule<'a>>>) {
     let rules = io::tokenize(rules, ",");
     for rule in &rules {
         if let Some((condition, dst)) = rule.split_once(':') {
-            let dst_action = get_action(dst);
-            let mut cat_arr = [0; 4];
-            let cond_rule = if let Some((cat, val)) = condition.split_once('<') {
-                cat_arr[cat_to_idx(cat)] = io::parse_num(val).unwrap();
-                Rule::IfLess(cat_arr, dst_action)
-            } else if let Some((cat, val)) = condition.split_once('>') {
-                cat_arr[cat_to_idx(cat)] = io::parse_num(val).unwrap();
-                Rule::IfMore(cat_arr, dst_action)
+            let dst_cond_action = get_action(dst);
+            let mut category_arr = [0; 4];
+            let cond_rule = if let Some((category, val)) = condition.split_once('<') {
+                category_arr[category_to_idx(category)] = io::parse_num(val).unwrap();
+                Rule::Conditional(category_arr, dst_cond_action, Cmp::Less)
+            } else if let Some((category, val)) = condition.split_once('>') {
+                category_arr[category_to_idx(category)] = io::parse_num(val).unwrap();
+                Rule::Conditional(category_arr, dst_cond_action, Cmp::More)
             } else {
                 panic!("No < or > found!")
             };
@@ -76,25 +84,19 @@ fn process_range_part<'a>(
 
     for rule in rules {
         match rule {
-            Rule::IfLess(cat, action) => {
-                let idx = (0..4).filter(|&i| cat[i] != 0).collect::<Vec<_>>()[0];
-                let range = Range { a: 1, b: cat[idx] };
-                let mut part_tmp = part.clone();
-                part_tmp[idx] = part[idx].intersect(&range).unwrap();
-                part[idx] = part[idx].difference(&range)[0];
+            Rule::Conditional(category, action, cmp) => {
+                let idx = (0..4).filter(|&i| category[i] != 0).collect::<Vec<_>>()[0];
+                let range = match cmp {
+                    Cmp::Less => Range {
+                        a: MIN_INC_RANGE,
+                        b: category[idx],
+                    },
+                    Cmp::More => Range {
+                        a: category[idx] + 1,
+                        b: MAX_EXC_RANGE,
+                    },
+                };
 
-                ret += match action {
-                    Action::Accepted => part_tmp.iter().map(|r| r.spread()).product::<i64>(),
-                    Action::Rejected => 0,
-                    Action::SendTo(dst) => process_range_part(&part_tmp, map, dst),
-                };
-            }
-            Rule::IfMore(cat, action) => {
-                let idx = (0..4).filter(|&i| cat[i] != 0).collect::<Vec<_>>()[0];
-                let range = Range {
-                    a: cat[idx] + 1,
-                    b: 4001,
-                };
                 let mut part_tmp = part.clone();
                 part_tmp[idx] = part[idx].intersect(&range).unwrap();
                 part[idx] = part[idx].difference(&range)[0];
@@ -121,19 +123,11 @@ fn process_part<'a>(part: [i64; 4], map: &HashMap<&'a str, Vec<Rule<'a>>>, start
     let rules = map.get(start).unwrap();
     for rule in rules {
         match rule {
-            Rule::IfLess(cat, action) => {
-                let idx = (0..4).filter(|&i| cat[i] != 0).collect::<Vec<_>>()[0];
-                if part[idx] < cat[idx] {
-                    return match action {
-                        Action::Accepted => part.iter().sum(),
-                        Action::Rejected => 0,
-                        Action::SendTo(dst) => process_part(part, map, dst),
-                    };
-                }
-            }
-            Rule::IfMore(cat, action) => {
-                let idx = (0..4).filter(|&i| cat[i] != 0).collect::<Vec<_>>()[0];
-                if part[idx] > cat[idx] {
+            Rule::Conditional(category, action, cmp) => {
+                let idx = (0..4).filter(|&i| category[i] != 0).collect::<Vec<_>>()[0];
+                if (part[idx] < category[idx] && cmp == &Cmp::Less)
+                    || (part[idx] > category[idx] && cmp == &Cmp::More)
+                {
                     return match action {
                         Action::Accepted => part.iter().sum(),
                         Action::Rejected => 0,
@@ -166,8 +160,8 @@ fn part1<'a>(input: &'a str) -> i64 {
         let cats = io::tokenize(&line[1..line.len() - 1], ",");
         assert!(cats.len() == 4);
         let mut part: [i64; 4] = [0; 4];
-        for (i, cat) in cats.iter().enumerate() {
-            let (_, v) = cat.split_once('=').unwrap();
+        for (i, category) in cats.iter().enumerate() {
+            let (_, v) = category.split_once('=').unwrap();
             part[i] = io::parse_num(v).unwrap();
         }
         ans += process_part(part, &map, "in");
@@ -185,9 +179,11 @@ fn part2<'a>(input: &'a str) -> i64 {
 
     let mut ranges: Vec<Range<i64>> = Vec::new();
     for _ in 0..4 {
-        ranges.push(Range { a: 1, b: 4001 });
+        ranges.push(Range {
+            a: MIN_INC_RANGE,
+            b: MAX_EXC_RANGE,
+        });
     }
-
     process_range_part(&ranges, &map, "in")
 }
 
