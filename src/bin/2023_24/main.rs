bcb00dda1624d63f6e2f9d91990970e7b58e2c34
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-26 19:42:58 +0100

    cleanup 2023.24 and 2023.25

--- src/bin/2023_24/main.rs
+++ src/bin/2023_24/main.rs
@@ -11,40 +11,46 @@ struct Hail {
 }
 
 impl Hail {
-
-    fn time_to_collision_2d(&self, other: &Hail, d1 : usize, d2 : usize) -> Option<(f64, f64)> {
-        let pd = self.p - other.p;
-        let den = self.v[d1] * other.v[d2] - other.v[d1] * self.v[d2];
-        if den.abs() < EPSILON {
-            return None;
+    fn pos_f(&self) -> DVec3 {
+        DVec3 {
+            x: self.p.x as f64,
+            y: self.p.y as f64,
+            z: self.p.z as f64,
         }
-
-        let num1 = other.v[d1] * pd[d2] as f64 - pd[d1] as f64 * other.v[d2];
-        let num2 = self.v[d1] * pd[d2] as f64 - pd[d1] as f64 * self.v[d2];
-
-        Some((num1 / den, num2 / den))
     }
 
-    fn time_to_collision_3d(&self, other: &Hail) -> Option<f64> {
-        let pd = other.p - self.p;
-        let pdf = DVec3{x : pd.x as f64, y : pd.y as f64, z : pd.z as f64};
-        let v2_cross_pd = other.v.cross(pdf).length();
-        let v2_cross_v1 = other.v.cross(self.v).length();
+    fn time_to_collision(&self, other: &Hail, two_d_only: bool) -> Option<(f64, f64)> {
+        // Solve p1 + v1 * t1 = p2 + v2 * t2 by taking cross products by v1 and v2
+        let p12 = self.pos_f() - other.pos_f();
+        let p12_v1 = p12.cross(self.v);
+        let p12_v2 = p12.cross(other.v);
+        let v21 = other.v.cross(self.v);
+        let v21_l = v21.length();
 
-        if v2_cross_v1 == 0.0 {
+        if v21_l < EPSILON {
             return None;
         }
-        Some(v2_cross_pd / v2_cross_v1)
+
+        if two_d_only {
+            return Some((p12_v2.z / v21.z, p12_v1.z / v21.z));
+        }
+
+        Some((p12_v2.length() / v21_l, p12_v1.length() / v21_l))
     }
 
     fn position_in_time(&self, time: f64) -> I64Vec3 {
-        self.p + I64Vec3{ x : (self.v.x * time) as i64, y : (self.v.y * time) as i64, z : (self.v.z * time) as i64 }
+        self.p
+            + I64Vec3 {
+                x: (self.v.x * time) as i64,
+                y: (self.v.y * time) as i64,
+                z: (self.v.z * time) as i64,
+            }
     }
 }
 
 fn intersect(hail1: &Hail, hail2: &Hail, limits: Option<(i64, i64)>) -> bool {
-    if let Some((t1, t2)) = hail1.time_to_collision_2d(&hail2, 0, 1) {
-        if t1 > 0.0&& t2 > 0.0 {
+    if let Some((t1, t2)) = hail1.time_to_collision(hail2, true) {
+        if t1 > 0.0 && t2 > 0.0 {
             if let Some((min_lim, max_lim)) = limits {
                 let pos = hail1.position_in_time(t1);
                 if pos.x >= min_lim && pos.x <= max_lim && pos.y >= min_lim && pos.y <= max_lim {
@@ -70,7 +76,6 @@ fn intersecting_pairs(hails: &Vec<Hail>, limits: Option<(i64, i64)>) -> usize {
             };
         }
     }
-
     ans
 }
 
@@ -102,9 +107,9 @@ fn part1(input: &str) -> usize {
     intersecting_pairs(&hail, Some((200000000000000, 400000000000000)))
 }
 
-fn test_rock_vel(rock_vel: &DVec3, hails: &Vec<Hail>) -> Option<I64Vec3> {
+fn test_rock_vel(rock_vel: &DVec3, hails: &[Hail]) -> Option<I64Vec3> {
     // Relative speed of all of the hail stones would be V - V_r
-    let mut hails_tmp = hails.clone();
+    let mut hails_tmp = hails.to_owned();
     for hail in &mut hails_tmp {
         hail.v -= *rock_vel;
     }
@@ -115,8 +120,8 @@ fn test_rock_vel(rock_vel: &DVec3, hails: &Vec<Hail>) -> Option<I64Vec3> {
 
     for i in 0..n {
         for j in i + 1..n {
-            let opt_tcs = hails_tmp[i].time_to_collision_3d(&hails_tmp[j]);
-            if let Some(mut t1) = opt_tcs {
+            let opt_tcs = hails_tmp[i].time_to_collision(&hails_tmp[j], false);
+            if let Some((mut t1, _)) = opt_tcs {
                 t1 = t1.round();
                 let p1 = hails_tmp[i].position_in_time(t1);
                 if let Some(ps) = pos {
@@ -126,8 +131,7 @@ fn test_rock_vel(rock_vel: &DVec3, hails: &Vec<Hail>) -> Option<I64Vec3> {
                 } else {
                     pos = Some(p1);
                 }
-                                
-            } 
+            }
         }
     }
     pos
@@ -161,7 +165,6 @@ fn part2(input: &str) -> i64 {
 
     const MAX_SPEED: i64 = 500;
     let bar = ProgressBar::new(8 * MAX_SPEED as u64 * MAX_SPEED as u64 * MAX_SPEED as u64);
-    let mut opt_rock_pos : Option<I64Vec3> = None;
     for vx in -MAX_SPEED..=MAX_SPEED {
         for vy in -MAX_SPEED..=MAX_SPEED {
             for vz in -MAX_SPEED..=MAX_SPEED {
@@ -173,18 +176,16 @@ fn part2(input: &str) -> i64 {
                     z: vz as f64,
                 };
 
-                opt_rock_pos = test_rock_vel(&rock_vel, &hails);
+                let opt_rock_pos = test_rock_vel(&rock_vel, &hails);
                 if let Some(rock_pos) = opt_rock_pos {
-                    // All intersections found!
-                    println!("Stone Pos {} Possible Ans {}", rock_pos, rock_pos[0] + rock_pos[1] + rock_pos[2]);
-                    println!("Stone Vel {}", rock_vel);
+                    return rock_pos.to_array().iter().sum();
                 } else {
                     continue;
                 }
             }
         }
     }
-    opt_rock_pos.unwrap().to_array().iter().sum()
+    0
 }
 
 fn main() {
