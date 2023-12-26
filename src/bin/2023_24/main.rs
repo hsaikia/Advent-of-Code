c44078474577dcecc6c9ef4f9823366c36944e18
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2023-12-26 08:36:25 +0100

    Day24 and  Day 25 2023

--- src/bin/2023_24/main.rs
+++ src/bin/2023_24/main.rs
@@ -1,66 +1,73 @@
-use std::{
-    collections::{HashMap, HashSet, VecDeque},
-    f64::EPSILON,
-};
+use indicatif::ProgressBar;
+use std::f64::EPSILON;
 
-use aoc::{
-    common,
-    grid::{CellDir, CellIndex, Grid},
-    io,
-};
-use glam::DVec3;
-use num::complex::ComplexFloat;
+use aoc::{common, io};
+use glam::{DVec3, I64Vec3};
 
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
 struct Hail {
-    p: DVec3,
+    p: I64Vec3,
     v: DVec3,
 }
 
 impl Hail {
-    fn time_to_collision(&self, other: &Hail) -> Option<(f64, f64)> {
-        let pd = self.p - other.p;
-        //println!("{}", pd);
 
-        let den = self.v.x * other.v.y - other.v.x * self.v.y;
+    fn time_to_collision_2d(&self, other: &Hail, d1 : usize, d2 : usize) -> Option<(f64, f64)> {
+        let pd = self.p - other.p;
+        let den = self.v[d1] * other.v[d2] - other.v[d1] * self.v[d2];
         if den.abs() < EPSILON {
             return None;
         }
 
-        let num1 = other.v.x * pd.y - pd.x * other.v.y;
-        let num2 = self.v.x * pd.y - pd.x * self.v.y;
+        let num1 = other.v[d1] * pd[d2] as f64 - pd[d1] as f64 * other.v[d2];
+        let num2 = self.v[d1] * pd[d2] as f64 - pd[d1] as f64 * self.v[d2];
 
         Some((num1 / den, num2 / den))
     }
 
-    fn position_in_time(&self, time: f64) -> DVec3 {
-        self.p + self.v * time
+    fn time_to_collision_3d(&self, other: &Hail) -> Option<f64> {
+        let pd = other.p - self.p;
+        let pdf = DVec3{x : pd.x as f64, y : pd.y as f64, z : pd.z as f64};
+        let v2_cross_pd = other.v.cross(pdf).length();
+        let v2_cross_v1 = other.v.cross(self.v).length();
+
+        if v2_cross_v1 == 0.0 {
+            return None;
+        }
+        Some(v2_cross_pd / v2_cross_v1)
+    }
+
+    fn position_in_time(&self, time: f64) -> I64Vec3 {
+        self.p + I64Vec3{ x : (self.v.x * time) as i64, y : (self.v.y * time) as i64, z : (self.v.z * time) as i64 }
     }
 }
 
-fn intersecting_pairs(hails: &Vec<Hail>, min_lim: f64, max_lim: f64) -> usize {
+fn intersect(hail1: &Hail, hail2: &Hail, limits: Option<(i64, i64)>) -> bool {
+    if let Some((t1, t2)) = hail1.time_to_collision_2d(&hail2, 0, 1) {
+        if t1 > 0.0&& t2 > 0.0 {
+            if let Some((min_lim, max_lim)) = limits {
+                let pos = hail1.position_in_time(t1);
+                if pos.x >= min_lim && pos.x <= max_lim && pos.y >= min_lim && pos.y <= max_lim {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn intersecting_pairs(hails: &Vec<Hail>, limits: Option<(i64, i64)>) -> usize {
     let mut ans = 0;
 
     for i in 0..hails.len() {
         for j in i + 1..hails.len() {
-            if let Some((t1, t2)) = hails[i].time_to_collision(&hails[j]) {
-                //let t = t.floor();
-                if t1 > 0.0 && t2 > 0.0 {
-                    let pos = hails[i].position_in_time(t1);
-                    //let pos2 = hails[j].position_in_time(t2);
-
-                    //assert_eq!(pos, pos2);
-
-                    if pos.x >= min_lim && pos.x <= max_lim && pos.y >= min_lim && pos.y <= max_lim
-                    {
-                        println!(
-                            "Hail {} {:?} and Hail {} {:?} are colliding  at x = {} y = {}",
-                            i, hails[i], j, hails[j], pos.x, pos.y
-                        );
-                        ans += 1
-                    }
-                }
-            }
+            ans += if intersect(&hails[i], &hails[j], limits) {
+                1
+            } else {
+                0
+            };
         }
     }
 
@@ -80,10 +87,69 @@ fn part1(input: &str) -> usize {
             .map(|s| s.trim().parse::<f64>().unwrap())
             .collect::<Vec<_>>();
         hail.push(Hail {
-            p: DVec3 {
-                x: ps[0] as f64,
-                y: ps[1] as f64,
-                z: ps[2] as f64,
+            p: I64Vec3 {
+                x: ps[0],
+                y: ps[1],
+                z: ps[2],
+            },
+            v: DVec3 {
+                x: vs[0] as f64,
+                y: vs[1] as f64,
+                z: vs[2] as f64,
+            },
+        });
+    }
+    intersecting_pairs(&hail, Some((200000000000000, 400000000000000)))
+}
+
+fn test_rock_vel(rock_vel: &DVec3, hails: &Vec<Hail>) -> Option<I64Vec3> {
+    // Relative speed of all of the hail stones would be V - V_r
+    let mut hails_tmp = hails.clone();
+    for hail in &mut hails_tmp {
+        hail.v -= *rock_vel;
+    }
+
+    // Test for intersections
+    let n = hails_tmp.len();
+    let mut pos = None;
+
+    for i in 0..n {
+        for j in i + 1..n {
+            let opt_tcs = hails_tmp[i].time_to_collision_3d(&hails_tmp[j]);
+            if let Some(mut t1) = opt_tcs {
+                t1 = t1.round();
+                let p1 = hails_tmp[i].position_in_time(t1);
+                if let Some(ps) = pos {
+                    if p1 != ps {
+                        return None;
+                    }
+                } else {
+                    pos = Some(p1);
+                }
+                                
+            } 
+        }
+    }
+    pos
+}
+
+fn part2(input: &str) -> i64 {
+    let mut hails: Vec<Hail> = Vec::new();
+    for line in input.lines() {
+        let (pos, vel) = line.split_once(" @ ").unwrap();
+        let ps = io::tokenize(pos, ", ")
+            .iter()
+            .map(|s| s.trim().parse::<i64>().unwrap())
+            .collect::<Vec<_>>();
+        let vs = io::tokenize(vel, ", ")
+            .iter()
+            .map(|s| s.trim().parse::<f64>().unwrap())
+            .collect::<Vec<_>>();
+        hails.push(Hail {
+            p: I64Vec3 {
+                x: ps[0],
+                y: ps[1],
+                z: ps[2],
             },
             v: DVec3 {
                 x: vs[0] as f64,
@@ -93,14 +159,36 @@ fn part1(input: &str) -> usize {
         });
     }
 
-    //println!("{:?}", hail);
+    const MAX_SPEED: i64 = 500;
+    let bar = ProgressBar::new(8 * MAX_SPEED as u64 * MAX_SPEED as u64 * MAX_SPEED as u64);
+    let mut opt_rock_pos : Option<I64Vec3> = None;
+    for vx in -MAX_SPEED..=MAX_SPEED {
+        for vy in -MAX_SPEED..=MAX_SPEED {
+            for vz in -MAX_SPEED..=MAX_SPEED {
+                bar.inc(1);
+                // This is the velocity of the rock V_r
+                let rock_vel = DVec3 {
+                    x: vx as f64,
+                    y: vy as f64,
+                    z: vz as f64,
+                };
 
-    //intersecting_pairs(&hail, 7.0, 27.0)
-    intersecting_pairs(&hail, 200000000000000.0, 400000000000000.0)
+                opt_rock_pos = test_rock_vel(&rock_vel, &hails);
+                if let Some(rock_pos) = opt_rock_pos {
+                    // All intersections found!
+                    println!("Stone Pos {} Possible Ans {}", rock_pos, rock_pos[0] + rock_pos[1] + rock_pos[2]);
+                    println!("Stone Vel {}", rock_vel);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+    opt_rock_pos.unwrap().to_array().iter().sum()
 }
 
 fn main() {
     let input = common::get_input();
     common::timed(&input, part1, true);
-    //common::timed(&input, part2, false);
+    common::timed(&input, part2, false);
 }
--- src/bin/2023_25/main.rs
+++ src/bin/2023_25/main.rs
@@ -1,18 +1,264 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 use aoc::{
-    common,
-    grid::{CellDir, CellIndex, Grid},
+    common::{self, HashMapVector},
     io,
 };
+use itertools::Itertools;
+use rand::Rng;
 
-fn part1(input: &str) -> usize {
-    let mut ans = 0;
-    ans
+fn bfs_cluster(node: usize, map: &HashMap<usize, Vec<usize>>, clusters: &mut Vec<Option<usize>>) {
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+    q.push_back((node, node));
+
+    while !q.is_empty() {
+        let (f, cl) = q.pop_front().unwrap();
+
+        if clusters[f].is_some() {
+            continue;
+        }
+
+        clusters[f] = Some(cl);
+        if let Some(v) = map.get(&f) {
+            for x in v {
+                q.push_back((*x, cl));
+            }
+        }
+    }
+}
+
+fn bfs_proper(start: usize, end: usize, map: &HashMap<usize, Vec<usize>>) -> Option<Vec<usize>> {
+    let mut q: VecDeque<(usize, Vec<usize>)> = VecDeque::new();
+    q.push_back((start, vec![start]));
+
+    let mut seen: HashSet<usize> = HashSet::new();
+
+    while !q.is_empty() {
+        let (node, path) = q.pop_front().unwrap();
+
+        if node == end {
+            return Some(path);
+        }
+
+        if seen.contains(&node) {
+            continue;
+        }
+        seen.insert(node);
+
+        if let Some(v) = map.get(&node) {
+            for x in v {
+                let mut path_tmp = path.clone();
+                path_tmp.push(*x);
+                q.push_back((*x, path_tmp));
+            }
+        }
+    }
+
+    None
+}
+
+fn part1_proper(input: &str) -> usize {
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let (from, dsts) = line.split_once(": ").unwrap();
+        let dsts = io::tokenize(dsts, " ");
+        for dst in dsts {
+            map.add_to_vector_hashmap(&from, dst);
+            map.add_to_vector_hashmap(&dst, from);
+        }
+    }
+
+    let mut idxs: HashMap<&str, usize> = HashMap::new();
+    let mut nodes: Vec<&str> = Vec::new();
+
+    for (k, v) in map.iter() {
+        //println!("{} => {:?} | {}", k, v, v.len());
+        idxs.insert(k, idxs.len());
+        nodes.push(k);
+        // let k_map = bfs(k, &map);
+        // println!("{:?}", k_map);
+    }
+
+    let mut idx_map: HashMap<usize, Vec<usize>> = HashMap::new();
+    for (k, v) in map.iter() {
+        idx_map.insert(
+            *idxs.get(k).unwrap(),
+            v.iter()
+                .map(|vv| *idxs.get(vv).unwrap())
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    let mut rng = rand::thread_rng();
+    let n = map.len();
+    let mut att = 0;
+    loop {
+        att += 1;
+        println!("Attempt {}", att);
+
+        let s = rng.gen_range(0..n);
+        let e = rng.gen_range(0..n);
+
+        if s == e {
+            continue;
+        }
+
+        println!("S {} E {}", nodes[s], nodes[e]);
+
+        let mut idx_map_tmp = idx_map.clone();
+
+        for p in 0..3 {
+            if let Some(path) = bfs_proper(s, e, &idx_map_tmp) {
+                println!("Path {:?}", path.iter().map(|v| nodes[*v]).join(","));
+                for i in 0..path.len() - 1 {
+                    println!(
+                        "Path {}. Removing Edge {}-{}",
+                        p,
+                        nodes[path[i]],
+                        nodes[path[i + 1]]
+                    );
+                    idx_map_tmp
+                        .entry(path[i])
+                        .and_modify(|v| v.retain(|x| *x != path[i + 1]));
+                    idx_map_tmp
+                        .entry(path[i + 1])
+                        .and_modify(|v| v.retain(|x| *x != path[i]));
+                }
+            } else {
+                continue;
+            }
+        }
+
+        let mut clusters = (0..n).into_iter().map(|_| None).collect_vec();
+        bfs_cluster(s, &idx_map_tmp, &mut clusters);
+        bfs_cluster(e, &idx_map_tmp, &mut clusters);
+
+        let cluster_ids = clusters.iter().unique().collect_vec();
+
+        if cluster_ids.len() != 2 {
+            continue;
+        }
+
+        let mut ans = 1;
+        for cl in cluster_ids {
+            ans *= clusters.iter().filter(|&x| x == cl).count();
+        }
+
+        return ans;
+    }
 }
 
 fn main() {
     let input = common::get_input();
-    common::timed(&input, part1, true);
+    common::timed(&input, part1_proper, true);
     //common::timed(&input, part2, false);
 }
+
+fn does_split(
+    map: &HashMap<usize, Vec<usize>>,
+    wires: &Vec<(usize, usize)>,
+    nodes: &Vec<&str>,
+    //) -> Option<(Vec<usize>, Vec<usize>)> {
+) -> Option<usize> {
+    let mut conn = Vec::new();
+    for _ in 0..map.len() {
+        conn.push(None);
+    }
+
+    // Make new map with wires removed
+    let mut map_new = map.clone();
+    for wire in wires {
+        map_new
+            .entry(wire.0)
+            .and_modify(|v| v.retain_mut(|x| *x != wire.1));
+        map_new
+            .entry(wire.1)
+            .and_modify(|v| v.retain_mut(|x| *x != wire.0));
+    }
+
+    let mut clusters = Vec::new();
+    for i in 0..map.len() {
+        if conn[i].is_none() {
+            clusters.push(i);
+            bfs_cluster(i, &map_new, &mut conn);
+        }
+    }
+    //println!("Split into {} groups. ", conn.iter().unique().count());
+
+    if conn.iter().unique().count() == 2 {
+        let mut ans = 1;
+        for cluster in clusters {
+            ans *= conn
+                .iter()
+                .enumerate()
+                .filter_map(|(i, x)| if cluster == x.unwrap() { Some(i) } else { None })
+                .count();
+        }
+        return Some(ans);
+    }
+
+    None
+}
+
+fn part1(input: &str) -> usize {
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let (from, dsts) = line.split_once(": ").unwrap();
+        let dsts = io::tokenize(dsts, " ");
+        for dst in dsts {
+            map.add_to_vector_hashmap(&from, dst);
+            map.add_to_vector_hashmap(&dst, from);
+        }
+    }
+
+    let mut idxs: HashMap<&str, usize> = HashMap::new();
+    let mut nodes: Vec<&str> = Vec::new();
+
+    for (k, v) in map.iter() {
+        //println!("{} => {:?} | {}", k, v, v.len());
+        idxs.insert(k, idxs.len());
+        nodes.push(k);
+        // let k_map = bfs(k, &map);
+        // println!("{:?}", k_map);
+    }
+
+    let mut idx_map: HashMap<usize, Vec<usize>> = HashMap::new();
+    for (k, v) in map.iter() {
+        idx_map.insert(
+            *idxs.get(k).unwrap(),
+            v.iter()
+                .map(|vv| *idxs.get(vv).unwrap())
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    let edges = idx_map
+        .iter()
+        .flat_map(|(k, v)| {
+            v.iter()
+                .filter_map(|x| if k < x { Some((*k, *x)) } else { None })
+                .collect::<Vec<_>>()
+        })
+        .collect_vec();
+
+    let mut c = 0;
+    for i in 0..edges.len() {
+        for j in i + 1..edges.len() {
+            for k in j + 1..edges.len() {
+                c += 1;
+                print!("Checking #{} ", c);
+                let wires = vec![edges[i], edges[j], edges[k]];
+                for wire in &wires {
+                    print!("{}-{} | ", nodes[wire.0], nodes[wire.1]);
+                }
+                println!();
+
+                if let Some(ans) = does_split(&idx_map, &wires, &nodes) {
+                    return ans;
+                }
+            }
+        }
+    }
+    0
+}
+
