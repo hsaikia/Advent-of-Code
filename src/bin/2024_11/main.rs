3eb0ca3f298358dd05f61fe24be014ba2b6fd8a0
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2024-12-11 06:54:49 +0100

    minor

--- src/bin/2024_11/main.rs
+++ src/bin/2024_11/main.rs
@@ -2,8 +2,11 @@ use std::collections::HashMap;
 
 use aoc::{common, io};
 
-fn reduce(input: &str) -> String {
-    let num: usize = io::parse_num(input);
+fn reduce(input: &str, mul: bool) -> String {
+    let mut num: usize = io::parse_num(input);
+    if mul {
+        num *= 2024;
+    }
     num.to_string()
 }
 
@@ -12,12 +15,10 @@ fn evolve(input: &str) -> Vec<String> {
     if input == "0" {
         ret.push("1".to_string());
     } else if input.len() % 2 == 0 {
-        ret.push(reduce(&input[0..input.len() / 2]));
-        ret.push(reduce(&input[input.len() / 2..]));
+        ret.push(reduce(&input[0..input.len() / 2], false));
+        ret.push(reduce(&input[input.len() / 2..], false));
     } else {
-        let num: usize = io::parse_num(input);
-        let new_num = 2024 * num;
-        ret.push(new_num.to_string());
+        ret.push(reduce(input, true));
     }
     ret
 }
@@ -32,8 +33,7 @@ fn num_stones(stone: &str, times: usize, cache: &mut HashMap<(String, usize), us
     let mut ans = 0;
     let next = evolve(stone);
     for ns in next.iter() {
-        let x = num_stones(ns, times - 1, cache);
-        ans += x;
+        ans += num_stones(ns, times - 1, cache);
     }
     cache.insert((stone.to_string(), times), ans);
     ans
