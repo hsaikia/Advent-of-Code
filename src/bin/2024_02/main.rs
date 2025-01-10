9445c38239653b5aa68f72e8b3b3f5608212dd68
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2025-01-10 14:59:09 +0100

    minor 2024_02

--- src/bin/2024_02/main.rs
+++ src/bin/2024_02/main.rs
@@ -2,14 +2,12 @@ use aoc::common;
 use aoc::io;
 
 fn good_difference(seq: &[u32]) -> bool {
-    const ALLOWED_DIFF_POS: [i64; 3] = [1, 2, 3];
-    const ALLOWED_DIFF_NEG: [i64; 3] = [-1, -2, -3];
     let diff_seq: Vec<i64> = seq
         .windows(2)
         .map(|w| i64::from(w[1]) - i64::from(w[0]))
         .collect();
-    diff_seq.iter().all(|x| ALLOWED_DIFF_POS.contains(x))
-        || diff_seq.iter().all(|x| ALLOWED_DIFF_NEG.contains(x))
+    diff_seq.iter().all(|x| *x >= 1 && *x <= 3)
+        || diff_seq.iter().all(|x| *x >= -3 && *x <= -1)
 }
 
 fn good_sequence<const PART: usize>(seq: &[u32]) -> bool {
