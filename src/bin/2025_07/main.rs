58f2d5d1d7400d2a0d88b8537b24f83e95dd6105
Himangshu Saikia <himangshu.saikia.iitg@gmail.com>
2025-12-07 07:36:02 +0100

    variables

--- src/bin/2025_07/main.rs
+++ src/bin/2025_07/main.rs
@@ -6,74 +6,82 @@ use aoc::{
 };
 
 fn solve2(input: &str) -> usize {
-    let mut ans = 0;
+    let mut ans2 = 0;
     let grid = Grid::from_str(input, |c| c);
     let mut pos = grid.positions(&'S');
     pos[0].0 += 1;
-    let tcs = grid.positions(&'^');
-    let emt = grid.positions(&'.');
+    let tachyons = grid.positions(&'^');
+    let empty_spaces = grid.positions(&'.');
 
-    let mut post = HashMap::new();
-    post.insert(pos[0], 1);
+    let mut space_time = HashMap::new();
+    space_time.insert(pos[0], 1);
 
-    while !post.is_empty() {
-        let mut new_post: HashMap<CellIndex, usize> = HashMap::new();
+    while !space_time.is_empty() {
+        let mut new_space_time: HashMap<CellIndex, usize> = HashMap::new();
 
-        for (p, t) in post {
-            if emt.contains(&p) {
+        for (p, t) in space_time {
+            if empty_spaces.contains(&p) {
                 let new_p = (p.0 + 1, p.1);
-                if let Some(x) = new_post.get_mut(&new_p) {
+                if let Some(x) = new_space_time.get_mut(&new_p) {
                     *x += t;
                 } else {
-                    new_post.insert(new_p, t);
+                    new_space_time.insert(new_p, t);
                 }
             }
-            if tcs.contains(&p) {
+            if tachyons.contains(&p) {
                 if p.1 > 0 {
                     let new_p = (p.0 + 1, p.1 - 1);
-                    if let Some(x) = new_post.get_mut(&new_p) {
+                    if let Some(x) = new_space_time.get_mut(&new_p) {
                         *x += t;
                     } else {
-                        new_post.insert(new_p, t);
+                        new_space_time.insert(new_p, t);
                     }
                 }
                 if p.1 + 1 < grid.cols {
                     let new_p = (p.0 + 1, p.1 + 1);
-                    if let Some(x) = new_post.get_mut(&new_p) {
+                    if let Some(x) = new_space_time.get_mut(&new_p) {
                         *x += t;
                     } else {
-                        new_post.insert(new_p, t);
+                        new_space_time.insert(new_p, t);
                     }
                 }
             }
         }
 
-        if new_post.is_empty() {
+        if new_space_time.is_empty() {
             break;
         }
 
-        ans = new_post.values().sum::<usize>();
-        post = new_post;
+        ans2 = new_space_time.values().sum::<usize>();
+        space_time = new_space_time;
     }
 
     //grid.print();
-    ans
+    ans2
 }
 
-fn solve<const PART: usize>(input: &str) -> usize {
+fn solve1(input: &str) -> usize {
     let mut ans = 0;
     let grid = Grid::from_str(input, |c| c);
     let mut pos = grid.positions(&'S');
     pos[0].0 += 1;
-    let tcs = grid.positions(&'^');
-    let emt = grid.positions(&'.');
+    let tachyons = grid.positions(&'^');
+    let empty_spaces = grid.positions(&'.');
 
     while !pos.is_empty() {
-        let cnt_emt: Vec<CellIndex> = emt.iter().filter(|x| pos.contains(x)).map(|x| *x).collect();
-        let cnt: Vec<CellIndex> = tcs.iter().filter(|x| pos.contains(x)).map(|x| *x).collect();
+        let cnt_empty_spaces: Vec<CellIndex> = empty_spaces
+            .iter()
+            .filter(|x| pos.contains(x))
+            .map(|x| *x)
+            .collect();
+        let cnt: Vec<CellIndex> = tachyons
+            .iter()
+            .filter(|x| pos.contains(x))
+            .map(|x| *x)
+            .collect();
         let mut new_pos = HashSet::new();
 
-        for c in &cnt_emt {
+        for c in &cnt_empty_spaces {
             new_pos.insert((c.0 + 1, c.1));
         }
 
@@ -95,7 +103,7 @@ fn solve<const PART: usize>(input: &str) -> usize {
 
 fn main() {
     if let Some(input) = common::get_input() {
-        common::timed(&input, solve::<1>, true);
+        common::timed(&input, solve1, true);
         common::timed(&input, solve2, false);
     }
 }
@@ -107,7 +115,7 @@ mod tests {
     #[test]
     fn test_samples() {
         let sample_input = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............";
-        assert_eq!(solve::<1>(sample_input), 21);
+        assert_eq!(solve1(sample_input), 21);
         assert_eq!(solve2(sample_input), 40);
     }
 }
